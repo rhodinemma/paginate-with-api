9e3468a4a31771fe570029a187e101bbc7bca4d9
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2021-08-25 20:10:17 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ReactPaginate from "react-paginate";
+
+function App() {
+  const [offset, setOffset] = useState(0);
+  const [data, setData] = useState([]);
+  const [perPage] = useState(10);
+  const [pageCount, setPageCount] = useState(0);
+
+  const getData = async () => {
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/photos`);
+    const data = res.data;
+    const slice = data.slice(offset, offset + perPage);
+    const postData = slice.map((pd) => (
+      <div key={pd.id}>
+        <p>{pd.title}</p>
+        <img src={pd.thumbnailUrl} alt="" />
+      </div>
+    ));
+    setData(postData);
+    setPageCount(Math.ceil(data.length / perPage));
+  };
+
+  const handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    setOffset(selectedPage + 1);
+  };
+
+  useEffect(() => {
+    getData()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [offset]);
+
+  return (
+    <div className="App">
+      {data}
+      <ReactPaginate
+        previousLabel={"prev"}
+        nextLabel={"next"}
+        breakLabel={"..."}
+        breakClassName={"break-me"}
+        pageCount={pageCount}
+        marginPageDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        subContainerClassName={"pages pagination"}
+        activeClassName={"active"}
+      />
+    </div>
+  );
+}
+
+export default App;
